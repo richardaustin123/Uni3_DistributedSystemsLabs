784fddfd0902435a1d231de2c38698e4a0735bb9
Richard Austin <richardaustin123@icloud.com>
2024-02-28 09:44:00 +0000

    Lab5 slide 1

---
+++ Lab5/Ex1.cpp
@@ -0,0 +1,36 @@
+// Aysnc 
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <future>
+
+using namespace std;
+
+int addValues(int *num1, int *num2, int *num3);
+
+int main() {
+    thread t1;
+
+    int num1, num2, num3;
+    int result;
+    future<int> res;
+
+    cout << "Enter number 1: ";
+    cin >> num1;
+    cout << "Enter number 2: ";
+    cin >> num2;
+    cout << "Enter number 3: ";
+    cin >> num3;
+
+    res = async(addValues,&num1, &num2, &num3); // reference with the &
+    result = res.get();
+
+    addValues(&num1, &num2, &num3);
+
+    cout << "Answer: " << result << endl;
+}
+
+int addValues(int *num1, int *num2, int *num3) { // dereference with the *
+    return *num1 + *num2 + *num3;
+}
